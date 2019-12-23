98486dfd2f610e8607f219c920c0cf07290c1d00
signbird <signbird@126.com>
2019-12-23 10:42:26 +0800

    join

--- src/main/java/org/bqftest/current/thread/Join.java
+++ src/main/java/org/bqftest/current/thread/Join.java
@@ -16,34 +16,79 @@ public final synchronized void join() throws InterruptedException {
 }
  */
 public class Join {
-	public static void main(String[] args) throws Exception {
-		Thread previous = Thread.currentThread();
-		for (int i = 0; i < 10; i++) {
-			// 每个线程拥有前一个线程的引用，需要等待前一个线程终止，才能从等待中返回
-			Thread thread = new Thread(new Domino(previous), String.valueOf(i));
-			thread.start();
-			previous = thread;
-		}
-		TimeUnit.SECONDS.sleep(10);
-		System.out.println(Thread.currentThread().getName() + " terminate.");
-	}
+    
+    
+    
+    public static void main(String[] args) throws InterruptedException {
+        Thread period = Thread.currentThread();
+        
+        for (int i = 0; i < 10; i++){
+            Thread t = new Thread(new Domino(period), String.valueOf(i));
+            t.start();
+            period = t;
+        }
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("all ready, go...");
+    }
+    
+    static class Domino implements Runnable{
 
-	// 多米诺骨牌
-	static class Domino implements Runnable {
-		private Thread thread;
-
-		public Domino(Thread thread) {
-			this.thread = thread;
-		}
-
-		public void run() {
-			try {
-				thread.join();
-				TimeUnit.SECONDS.sleep(2);
-			} catch (InterruptedException e) {
-			}
-			
-			System.out.println(Thread.currentThread().getName() + " terminate.");
-		}
-	}
+        private Thread period;
+        
+        public Domino(Thread t){
+            this.period = t;
+        }
+        
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " ready.");
+            
+            try {
+                period.join();
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            
+            System.out.println(Thread.currentThread().getName() + " down.");
+        }
+        
+    } 
+    
+    
+//    
+//    
+//    
+//    
+//    
+//	public static void main(String[] args) throws Exception {
+//		Thread previous = Thread.currentThread();
+//		for (int i = 0; i < 10; i++) {
+//			// 每个线程拥有前一个线程的引用，需要等待前一个线程终止，才能从等待中返回
+//			Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+//			thread.start();
+//			previous = thread;
+//		}
+//		TimeUnit.SECONDS.sleep(10);
+//		System.out.println(Thread.currentThread().getName() + " terminate.");
+//	}
+//
+//	// 多米诺骨牌
+//	static class Domino implements Runnable {
+//		private Thread thread;
+//
+//		public Domino(Thread thread) {
+//			this.thread = thread;
+//		}
+//
+//		public void run() {
+//			try {
+//				thread.join();
+//				TimeUnit.SECONDS.sleep(2);
+//			} catch (InterruptedException e) {
+//			}
+//			
+//			System.out.println(Thread.currentThread().getName() + " terminate.");
+//		}
+//	}
 }
